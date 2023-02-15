f77eb1a0ed2e5fa5438a8aa84ed37c289564ef52
StebenC0912 <stebenc09122002@gmail.com>
2023-02-15 21:31:19 +0700

    tut02

--- firstapp/app/src/main/java/com/example/mynetflix/MainActivity.java
+++ firstapp/app/src/main/java/com/example/mynetflix/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.mynetflix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public void onClick(View view) {
+        Log.i("Info", "onClick: Logging");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button LoginBtn = findViewById(R.id.LoginBtn);
+        LoginBtn.setText("Login");
+        String text = LoginBtn.getText().toString();
     }
 }
